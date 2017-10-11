81ead8223f50da5f88e9be711f6082124424f4de
Ramil Nugmanov <stsouko@live.ru>
2017-10-12 00:51:45 +0300

    table merging done

--- BioStat/views/prepare.py
+++ BioStat/views/prepare.py
@@ -22,8 +22,8 @@
 from collections import defaultdict
 from flask import url_for, render_template, redirect
 from flask.views import View
-from itertools import combinations, chain
-from pandas import read_csv, DataFrame
+from itertools import combinations
+from pandas import read_csv, DataFrame, MultiIndex, merge, concat
 from scipy.stats import wilcoxon, mannwhitneyu
 from werkzeug.utils import secure_filename
 from ..config import LAB_NAME, UPLOAD_ROOT, RESULTS_ROOT
@@ -46,45 +46,78 @@ class PrepareView(View):
         header = list(read_csv(data_path.as_posix(), nrows=1).columns)
         active_form = Prepare(header)
         if active_form.validate_on_submit():
-            df = read_csv(data_path.as_posix())
-            paired, unpaired = defaultdict(dict), defaultdict(dict)
+            data_fields = active_form.data.data
+            group_fields = active_form.group.data
+            date_field = active_form.date.data
+            gi = [date_field] + group_fields
 
-            gi = [active_form.date.data] + active_form.group.data
-            mms = df.groupby(gi).agg(['mean', 'median', 'std'])
+            df = read_csv(data_path.as_posix(), usecols=gi + data_fields)
+            mms = df.groupby(gi).agg(['mean', 'median', 'std']).reset_index()
+            mms.columns = mms.columns.swaplevel()
+
+            paired, unpaired = defaultdict(dict), defaultdict(dict)
             for (d, *g), v in df.groupby(gi):
                 g = tuple(g)
                 v.set_index(gi, inplace=True)
                 paired[g][d] = unpaired[d][g] = v  # .dropna()
 
-            group_p = {}
-            for k, v in paired.items():
-                group_p[k] = day_p = {}
-                for x, y in combinations(v, 2):
-                    day_p[(x, y)] = data_p = {}
-                    for g in active_form.data.data:
-                        _, p_value = wilcoxon(v[x][g], v[y][g], zero_method='wilcox', correction=True)
-                        data_p[g] = p_value
+            group_p = []
+            for g, dv in paired.items():
+                for x, y in combinations(dv, 2):
+                    tmp = {'%s_1' % date_field: x, '%s_2' % date_field: y}
+                    tmp.update(zip(group_fields, g))
+                    group_p.append(tmp)
+                    for f in data_fields:
+                        _, p_value = wilcoxon(dv[x][f], dv[y][f], zero_method='wilcox', correction=True)
+                        tmp[f] = p_value
+            paired = DataFrame(group_p)
+            paired.columns = MultiIndex.from_tuples([('p_value', x) if x in data_fields else ('', x)
+                                                     for x in paired.columns])
 
-            paired = DataFrame({tuple(chain(i, m)): n for i, j in group_p.items() for m, n in j.items()}).T
-            paired.index.names = active_form.group.data + ['Day_1', 'Day_2']
+            group_u = []
+            for d, gv in unpaired.items():
+                for x, y in combinations(gv, 2):
+                    tmp = {date_field: d}
+                    tmp.update(zip(('%s_%d' % (y, x) for x in (1, 2) for y in group_fields), x + y))
+                    group_u.append(tmp)
+                    for f in data_fields:
+                        _, p_value = mannwhitneyu(gv[x][f], gv[y][f], use_continuity=True)
+                        tmp[f] = p_value
+            unpaired = DataFrame(group_u)
+            unpaired.columns = MultiIndex.from_tuples([('p_value', x) if x in data_fields else ('', x)
+                                                       for x in unpaired.columns])
 
-            day_u = {}
-            for k, v in unpaired.items():
-                day_u[k] = group_u = {}
-                for x, y in combinations(v, 2):
-                    group_u[(x, y)] = data_u = {}
-                    for g in active_form.data.data:
-                        _, p_value = mannwhitneyu(v[x][g], v[y][g], use_continuity=True)
-                        data_u[g] = p_value
-            unpaired = DataFrame({tuple(chain((i,), *m)): n for i, j in day_u.items() for m, n in j.items()}).T
-            unpaired.index.names = ['Day'] + ['%s_%d' % (y, x) for x in (1, 2) for y in active_form.group.data]
+            rgi = [('', x) for x in gi]
+            rgf = [('', x) for x in group_fields]
 
-            print(paired)
-            print(unpaired)
-            print(mms)
+            tmp = merge(mms, paired, left_on=rgi, right_on=[('', '%s_1' % date_field)] + rgf)
+            tmp.pop(('', date_field))
+            paired_mms = merge(mms, tmp, left_on=rgi, right_on=[('', '%s_2' % date_field)] + rgf, suffixes=('_2', '_1'))
+            paired_mms.pop(('', date_field))
+            for x in group_fields:
+                paired_mms[('', '%s_1' % x)] = paired_mms[('', '%s_2' % x)] = paired_mms.pop(('', x))
 
-            with (RESULTS_ROOT / data).open('w'):
-                pass
+            tmp = merge(mms, unpaired, left_on=rgi,
+                        right_on=[('', date_field)] + [('', '%s_1' % x) for x in group_fields])
+            for x in rgf:
+                tmp.pop(x)
+            unpaired_mms = merge(mms, tmp, left_on=rgi, suffixes=('_2', '_1'),
+                                 right_on=[('', date_field)] + [('', '%s_2' % x) for x in group_fields])
+            for x in rgf:
+                unpaired_mms.pop(x)
+            unpaired_mms[('', '%s_1' % date_field)] = unpaired_mms[('', '%s_2' % date_field)] = \
+                unpaired_mms.pop(('', date_field))
+
+            paired_mms[('', 'Type')] = 'paired'
+            unpaired_mms[('', 'Type')] = 'unpaired'
+            paired_mms.columns = paired_mms.columns.swaplevel()
+            unpaired_mms.columns = unpaired_mms.columns.swaplevel()
+
+            paired_mms = DataFrame(paired_mms.to_dict())
+            unpaired_mms = DataFrame(unpaired_mms.to_dict())
+
+            with (RESULTS_ROOT / data).open('w') as f:
+                concat([paired_mms, unpaired_mms]).to_csv(f)
 
             return redirect(url_for('.download', data=data))
 
