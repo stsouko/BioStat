c3201e7fd60b2bad65aec5233ed502c7ce5609a4
stsouko <stsouko@live.ru>
2017-10-13 18:07:32 +0300

    fixes.

--- BioStat/views/prepare.py
+++ BioStat/views/prepare.py
@@ -52,6 +52,10 @@ class PrepareView(View):
             date_field = active_form.date.data
             gi = [date_field] + group_fields
 
+            rgi = [('', x) for x in gi]
+            rgi_1 = [('', '%s_1' % x) for x in gi]
+            rgi_2 = [('', '%s_2' % x) for x in gi]
+
             df = read_csv(data_path.as_posix(), usecols=gi + data_fields)
             mms = df.groupby(gi).agg(['mean', 'median', 'std']).reset_index()
             mms.columns = mms.columns.swaplevel()
@@ -60,29 +64,60 @@ class PrepareView(View):
             for (d, *g), v in df.groupby(gi):
                 g = tuple(g)
                 v.set_index(gi, inplace=True)
-                paired[g][d] = unpaired[d][g] = v  # .dropna()
-
-            group_p = []
-            for g, dv in paired.items():
-                dgf = dict(zip(('%s_%d' % (y, x) for x in (1, 2) for y in group_fields), g * 2))
-                dgf['Type'] = 'Paired Wilcoxon'
-                for x, y in combinations(dv, 2):
-                    tmp = {'%s_1' % date_field: x, '%s_2' % date_field: y}
-                    tmp.update(dgf)
-                    group_p.append(tmp)
-                    for f in data_fields:
-                        _, p_value = wilcoxon(dv[x][f], dv[y][f], zero_method='wilcox', correction=True)
-                        tmp[f] = p_value
-            paired = DataFrame(group_p)
-            paired.columns = MultiIndex.from_tuples([('p-value', x) if x in data_fields else ('', x)
-                                                     for x in paired.columns])
-            for x in data_fields:
-                reject, p_value_adj, *_ = multipletests(paired[('p-value', x)], alpha=0.05, method='fdr_bh',
-                                                        is_sorted=False, returnsorted=False)
-                paired[('p-value_adj', x)] = p_value_adj
-                paired[('significance', x)] = reject
+                paired[g][d] = unpaired[d][g] = v
 
             group_u = []
+
+            if len(unpaired) > 1:
+                group_p = []
+                for g, dv in paired.items():
+                    dgf = dict(zip(('%s_%d' % (y, x) for x in (1, 2) for y in group_fields), g * 2))
+                    for x, y in combinations(dv, 2):
+                        tmp1 = {}
+                        tmp2 = {}
+
+                        for f in data_fields:
+                            vxf, vyf = dv[x][f], dv[y][f]
+                            loc = ~(vxf.isnull().as_matrix() | vyf.isnull().as_matrix())
+                            if loc.mean() > .0001:
+                                _, p_value = wilcoxon(vxf.loc[loc], vyf.loc[loc], zero_method='wilcox', correction=True)
+                                tmp1[f] = p_value
+                            else:
+                                try:
+                                    _, p_value = mannwhitneyu(vxf.dropna(), vyf.dropna(), use_continuity=True)
+                                except ValueError:
+                                    p_value = .5
+                                tmp2[f] = p_value * 2
+                        if tmp1:
+                            tmp1.update(dgf)
+                            tmp1.update({'%s_1' % date_field: x, '%s_2' % date_field: y, 'Type': 'Paired Wilcoxon'})
+                            group_p.append(tmp1)
+                        if tmp2:
+                            tmp2.update(dgf)
+                            tmp2.update({'%s_1' % date_field: x, '%s_2' % date_field: y, 'Type': 'Unpaired Wilcoxon'})
+                            group_u.append(tmp2)
+
+                paired = DataFrame(group_p)
+                paired.columns = MultiIndex.from_tuples([('p-value', x) if x in data_fields else ('', x)
+                                                         for x in paired.columns])
+                for x in data_fields:
+                    reject, p_value_adj, *_ = multipletests(paired[('p-value', x)], alpha=0.05, method='fdr_bh',
+                                                            is_sorted=False, returnsorted=False)
+                    paired[('p-value_adj', x)] = p_value_adj
+                    paired[('significance', x)] = reject
+
+                tmp = merge(mms, paired, left_on=rgi, right_on=rgi_1)
+                for x in rgi:
+                    tmp.pop(x)
+                paired_mms = merge(mms, tmp, left_on=rgi, right_on=rgi_2, suffixes=('_2', '_1'))
+                for x in rgi:
+                    paired_mms.pop(x)
+
+                paired_mms.columns = paired_mms.columns.swaplevel()
+                paired_mms = DataFrame(paired_mms.to_dict())
+            else:
+                paired_mms = DataFrame()
+
             for d, gv in unpaired.items():
                 dfd = {'%s_1' % date_field: d, '%s_2' % date_field: d, 'Type': 'Unpaired Wilcoxon'}
                 for x, y in combinations(gv, 2):
@@ -90,8 +125,12 @@ class PrepareView(View):
                     tmp.update(dfd)
                     group_u.append(tmp)
                     for f in data_fields:
-                        _, p_value = mannwhitneyu(gv[x][f], gv[y][f], use_continuity=True)
-                        tmp[f] = p_value
+                        try:
+                            _, p_value = mannwhitneyu(gv[x][f].dropna(), gv[y][f].dropna(), use_continuity=True)
+                        except ValueError:
+                            p_value = .5
+                        tmp[f] = p_value * 2
+
             unpaired = DataFrame(group_u)
             unpaired.columns = MultiIndex.from_tuples([('p-value', x) if x in data_fields else ('', x)
                                                        for x in unpaired.columns])
@@ -102,17 +141,6 @@ class PrepareView(View):
                     unpaired.loc[unpaired[('', '%s_1' % date_field)] == d, ('p-value_adj', x)] = p_value_adj
                     unpaired.loc[unpaired[('', '%s_1' % date_field)] == d, ('significance', x)] = reject
 
-            rgi = [('', x) for x in gi]
-            rgi_1 = [('', '%s_1' % x) for x in gi]
-            rgi_2 = [('', '%s_2' % x) for x in gi]
-
-            tmp = merge(mms, paired, left_on=rgi, right_on=rgi_1)
-            for x in rgi:
-                tmp.pop(x)
-            paired_mms = merge(mms, tmp, left_on=rgi, right_on=rgi_2, suffixes=('_2', '_1'))
-            for x in rgi:
-                paired_mms.pop(x)
-
             tmp = merge(mms, unpaired, left_on=rgi, right_on=rgi_1)
             for x in rgi:
                 tmp.pop(x)
@@ -120,15 +148,12 @@ class PrepareView(View):
             for x in rgi:
                 unpaired_mms.pop(x)
 
-            paired_mms.columns = paired_mms.columns.swaplevel()
             unpaired_mms.columns = unpaired_mms.columns.swaplevel()
-
-            paired_mms = DataFrame(paired_mms.to_dict())
             unpaired_mms = DataFrame(unpaired_mms.to_dict())
 
             result = concat([paired_mms, unpaired_mms])
             result.index = range(len(result.index))
-            result = result[list(chain((('%s_%d' % (y, x), '') for x in (1, 2) for y in gi),
+            result = result[list(chain((('%s_%d' % (y, x), '') for x in (1, 2) for y in gi), (('Type', ''), ),
                                        ((x, y) for x in data_fields for y in
                                         chain(('%s_%d' % (y, x) for x in (1, 2) for y in ('median', 'mean', 'std')),
                                               ('p-value', 'p-value_adj', 'significance')))))]
